41c06fd01459f67f08d6b5c0359674a3a7b5f302
Bruno Amorim <bruno.amorim032@gmail.com>
2021-06-07 19:53:52 -0300

    update

---
+++ Algoritmos de programaçao/17-05/livraria.py
@@ -0,0 +1,6 @@
+#coding: utf-8
+import os
+os.chdir("C:")
+os.chdir("C:\\Users\\bruno\\Downloads\\instantclient_19_6")
+
+import cx_Oracle
---
+++ Algoritmos de programaçao/24-05-2021/numerosint.py
@@ -0,0 +1,8 @@
+def quantosElementosSimples(lista):
+    qts = 0
+
+    for elemento in lista:
+        if type(elemento) is not lista
+        qts += 1
+        else:
+            qnts += quantosElementosSimples(elemento)
